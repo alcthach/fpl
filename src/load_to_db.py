ab795f921b24abae54a79ee5d11e0650a67f3b23
Alex Thach <alext>
2022-08-05 17:02:56 -0400

    WIP: Working on loading 'events' json to SQL table, blocker: element in tuple passed as an argument in SQL insert statement is a list of dictionaries, will have to find a way to cast this as a string, was also thrown a NameError when attempting this... Maybe wrapping in  might do the trick...

--- src/load_to_db.py
+++ src/load_to_db.py
@@ -5,19 +5,27 @@ Alex Thach - alcthach@gmail.com
 """
 
 import psycopg2
+from psycopg2.extras import Json 
 import json
 
-file_to_load = "../data/data_fpl_transfers.txt"
+file_to_load = "../data/data_events.txt" # Was transfers, but switched to events for testing
+
+# Config file to pull column list and insert scripts
+config_file = "fpl_config.json"
+config = open(config_file)
+config_data = json.load(config)
+
+
 
 # TODO: Refactor line below into the config file, load the config file in def main(); Re: more dynamic  
-insert_script = "INSERT into transfers (id, first_name, second_name, selected_by_percent, transfers_in, transfers_out, request_ts) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+# insert_script = "INSERT into transfers (id, first_name, second_name, selected_by_percent, transfers_in, transfers_out, request_ts) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 
-def load_postgresql(file_to_load):
+def load_transfers_postgresql(file_to_load):
 
     conn = psycopg2.connect("dbname=fpl user=postgres") 
     cur = conn.cursor()
 
-    transactions_sql = insert_script
+    transactions_sql = config_data['get_bootstrap_static']['events']['insert_script'] 
 
     with open(file_to_load) as transfers:
         data = json.load(transfers)
@@ -37,8 +45,34 @@ def load_postgresql(file_to_load):
 
     conn.close()
 
+
+def load_events_postgresql(file_to_load):
+
+    conn = psycopg2.connect("dbname=fpl user=postgres") 
+    cur = conn.cursor()
+
+    transactions_sql = config_data['get_bootstrap_static']['events']['insert_script']
+
+    with open(file_to_load) as events:
+        data = json.load(events)
+        
+    rows_added = 0
+
+    for row in data:
+        insert_data = tuple([*row.values()])    
+        print((transactions_sql, tuple(insert_data)))
+        # cur.execute(transactions_sql, insert_data)
+        rows_added += 1
+
+    print(f"Added {rows_added} row to transfers table!")
+
+    conn.commit()
+
+    conn.close()
+
+
 def main():
-    load_postgresql(file_to_load)
+    load_events_postgresql(file_to_load)
 
 if __name__ == "__main__":
     main()    
