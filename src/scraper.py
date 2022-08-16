8b830c43d19c9b962e24b67a09a0377a7f35d999
Alex Thach <alext>
2022-08-16 10:23:57 -0400

    WIP: Working, but requires refactoring and additional functions for value and transfers fact tables

--- src/scraper.py
+++ src/scraper.py
@@ -24,7 +24,7 @@ class Scraper:
         self.data = request.json()[self.key]
         return self.data
 
-    def filter_results(self, data):
+    def get_transfers_snapshot(self, data):
 
         current_time = datetime.datetime.now().isoformat()
 
@@ -32,23 +32,20 @@ class Scraper:
 
         for element in data:
             filtered_elements['filtered_elements'].append({'id' : element['id'],
-                                                                'first_name': element['first_name'],
-                                                                'second_name': element['second_name'],
-                                                                'selected_by_percent': element['selected_by_percent'],
-                                                                'transfers_in': element['transfers_in'],
-                                                                'transfers_out': element['transfers_out'],
-                                                                'api_request_ts': current_time}) # No need to to run datetime.datetime.now() for ever loop, seems wasteful to do so
+                                                           'transfers_in': element['transfers_in'],
+                                                           'transfers_out': element['transfers_out'],
+                                                           'timestamp': current_time}) # No need to to run datetime.datetime.now() for ever loop, seems wasteful to do so
 
         return filtered_elements
 
 
     def main(self, p_config_file): # Q: Why is config_file prefixed with "_p"?
         data = self.get_results(p_config_file) # Why p_config_file and not config_data?
-        data = self.filter_results(data)
-        pp = pprint.PrettyPrinter()
+        # data = self.filter_results(data)
+        pp = pprint.PrettyPrinter(indent=4, sort_dicts=False)
         pp.pprint(data)
         # export_file = open(f"../data/data_fpl_transfers_{datetime.datetime.now().isoformat()}.txt" , "w")
-        export_file = open(f"../data/data_fpl_transfers.txt", "w") 
+        export_file = open(f"../data/{self.api_call_type}_{self.key}.txt", "w") 
         json.dump(data, export_file)
         export_file.close()
 
@@ -61,6 +58,13 @@ class Scraper:
         config_data = json.load(config)
         self.main(config_data) # main() is called when the class is initialized? Yes
 
-Scraper('get_bootstrap_static', 'elements')
+# Let's just go with hard-coded method for now, I'll make revisions later on
+# Just want to get it working for now!
+# TODO Make sure to have timestamp columns for all the tables!
+# Going to need a different script for batch loads, this one can be an initial load
+keys_lst = ['events', 'game_settings', 'phases', 'teams', 'total_players', 'elements', 'element_stats', 'element_types']
+
+for key in keys_lst:
+    Scraper('get_bootstrap_static', key)
 
 # TODO continue building the rest of the scraper, grab other end points as well!
