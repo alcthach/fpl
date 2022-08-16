870f486189857238913291b4a5ba4ebd46eb2a03
Alex Thach <alcthach@gmail.com>
2022-08-16 11:27:58 -0400

    WIP: 'events' data loads to db successfully, Re: convert 'chip_plays' from type list to str, and 'top_element_info' from dict to str

--- src/load_to_db.py
+++ src/load_to_db.py
@@ -58,10 +58,36 @@ def load_events_postgresql(file_to_load):
         
     rows_added = 0
 
+    
+
     for row in data:
-        insert_data = tuple([*row.values()])    
+        insert_data = [ ]
+        raw_data = [*row.values()]
+        # insert_data = tuple([*row.values()])    
+
+        '''
+        PSEUDOCODE
+        if the value is not type list
+            then append the value to the list of values to insert table
+        other if the value is a type list
+            then convert it to str
+            add double quotes around it
+            and append the transformed to the list of values to insert into table
+        take the list of values to insert and convert it to a tuple
+        pass the tuple as an argument in the cur.execute(insert statement)
+        '''
+
+        for i in raw_data:
+            if type(i) not in [list, dict]:
+                insert_data.append(i)
+            else:
+                list_to_str = str(i)
+                transformed_str = list_to_str 
+                insert_data.append(transformed_str)
+
+                
         print((transactions_sql, tuple(insert_data)))
-        # cur.execute(transactions_sql, insert_data)
+        cur.execute(transactions_sql, tuple(insert_data))
         rows_added += 1
 
     print(f"Added {rows_added} row to transfers table!")
