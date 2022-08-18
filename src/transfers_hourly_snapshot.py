61d8097ec75d40de2167bbef0c09574729aad91b
Alex Thach <alcthach@gmail.com>
2022-08-18 15:28:20 -0400

    Initial commit of transfers snapshot data pull, to be loaded onto transfers snapshot fact table in future

---
+++ src/transfers_hourly_snapshot.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import json
+import datetime
+from scraper import Scraper
+
+# TODO Adjust timestamp to have up to the minute?
+
+def get_timestamp():
+    timestamp = datetime.datetime.now().isoformat()     
+    return timestamp  
+
+def write_to_file(data):
+    timestamp = get_timestamp()
+    with open(f"../data/transfers_hourly_snapshot_{get_timestamp()}.txt", "w") as export_file:
+        json.dump(data, export_file)
+
+def get_transfers_snapshot(data):
+    transfers_data = [ ] 
+    for player in data:
+       transfers_data.append(
+                            {'id' : player['id'],
+                             'transfers_in': player['transfers_in'],
+                             'transfers_out': player['transfers_out'],
+                             'timestamp': get_timestamp()})
+    return transfers_data
+
+def main():
+    Players = Scraper('get_bootstrap_static', 'elements')
+    data = Players.data    
+    transfers_data = get_transfers_snapshot(data)
+    write_to_file(transfers_data)
+
+if __name__ == "__main__":
+    main()
