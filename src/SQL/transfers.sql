95214774e9c21c5cb6ac7089a006f24afe4f3a3c
athach <alcthach@gmail.com>
2022-09-06 21:03:59 -0400

    initial commit

---
+++ src/SQL/transfers.sql
@@ -0,0 +1,18 @@
+SELECT first_name, SUM(transfers_in_events)
+FROM
+(
+	SELECT first_name, 
+			second_name,
+			thhs.transfers_in,
+			thhs.transfers_in_events,
+			thhs.transfers_out,
+			thhs.transfers_out_events,
+			thhs.transfers_in - thhs.transfers_out AS net_transfers,
+			thhs.transfers_in_events - thhs.transfers_out_events AS net_transfers_event,
+			thhs.timestamp
+	FROM elements e 
+	JOIN transfers_half_hour_snapshot thhs 
+	ON e.id = thhs.id 
+	WHERE first_name = 'Erling'
+	ORDER BY timestamp) AS erling_haaland_transfers
+GROUP BY first_name;
